cc90409e0954c218394427cabe633801710a9a8d
sqrooch <sqrooch@yandex.ru>
2022-06-30 19:26:29 +0300

    Создал проект и приступил к написанию кода.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(calc("abc"));
+    }
+    public static String calc(String input)
+    {
+        return input;
+    }
+}
